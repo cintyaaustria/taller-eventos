a68726e2eab69fb2d922129db76a891d26fd4d6d
Cintya <you@example.com>
2025-08-15 22:12:49 -0300

    cambios en HTML, JS y CSS

---
+++ eventos.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+let div=document.getElementById("iddiv");
+div.addEventListener("click",function(){
+
+    alert("Este es el div");
+});
+});
